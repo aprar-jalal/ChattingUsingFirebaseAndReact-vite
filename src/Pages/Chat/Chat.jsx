05bfbd38161842f9837dfdd91ee7d2008ed9c643
aprar-jalal <s12324682@stu.najah.edu>
2026-07-19 06:07:19 -0700

    chat

---
+++ src/Pages/Chat/Chat.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Chat.module.css"
+import Navbar from '../../Components/Navbar'
+function Chat() {
+  
+  return (
+    <div className='Container'>
+      <div></div>
+      <div className='ChatArea'>
+        <Navbar/>
+      <p>rrr</p>
+      </div>
+    </div>
+  )
+}
+
+export default Chat
